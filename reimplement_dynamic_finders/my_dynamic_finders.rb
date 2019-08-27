c7f3b232f00aef0e155595cb45b63ec037859a2d
hamadanaoto <teiru_3@yahoo.co.jp>
2019-08-27 21:20:35 +0900

    add my_dynamic_finders

--- reimplement_dynamic_finders/my_dynamic_finders.rb
+++ reimplement_dynamic_finders/my_dynamic_finders.rb
@@ -1,2 +1,20 @@
 module MyDynamicFinders
+  def method_missing(name, *args)
+    if name.to_s =~ /find_by_(.+)/
+      return super unless User.method_defined?($1)
+
+      $DATABASE.find {|u| u.public_send($1) == args[0] }
+    else
+      super
+    end
+  end
+
+  def respond_to_missing?(symbol, include_private)
+    if name.to_s =~ /find_by_(.+)/
+      return super unless User.method_defined?($1)
+      true
+    else
+      false
+    end
+  end
 end
