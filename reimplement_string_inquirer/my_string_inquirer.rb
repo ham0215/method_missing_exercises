66cea63b23a884bb1ad6b9b1b6694d78a7800156
hamadanaoto <teiru_3@yahoo.co.jp>
2019-08-27 21:20:02 +0900

    add my_string_inquirer

--- reimplement_string_inquirer/my_string_inquirer.rb
+++ reimplement_string_inquirer/my_string_inquirer.rb
@@ -1,2 +1,13 @@
-class MyStringInquirer
+class MyStringInquirer < String
+  def method_missing(name)
+    if name.to_s.end_with?('?')
+      self == name[0..-2]
+    else
+      super
+    end
+  end
+
+  def respond_to_missing?(symbol, include_private)
+    symbol.to_s.end_with?('?')
+  end
 end
