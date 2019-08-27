d15c5e4e8c4a97417757fdb40c805f48b3a1d8e6
hamadanaoto <teiru_3@yahoo.co.jp>
2019-08-27 21:33:40 +0900

    add find_by!

--- reimplement_dynamic_finders/my_dynamic_finders.rb
+++ reimplement_dynamic_finders/my_dynamic_finders.rb
@@ -1,24 +1,30 @@
 module MyDynamicFinders
   def method_missing(name, *args)
-    if name.to_s =~ /find_by_(.+)/
-      cols = $1.split('_and_')
+    if name.to_s =~ /^find_by_(.+)$/
+      hit = $1.end_with?('!')
+      str = hit ? $1.delete('!') : $1
+      cols = str.split('_and_')
       return super unless cols.all? {|c| User.method_defined?(c) }
 
-      $DATABASE.find do |u|
+      u = $DATABASE.find do |u|
         i = -1
         cols.all? do |c|
           i += 1
           u.public_send(c) == args[i]
         end
       end
+      raise unless u
+      u
     else
       super
     end
   end
 
   def respond_to_missing?(symbol, include_private)
-    if name.to_s =~ /find_by_(.+)/
-      cols = $1.split('_and_')
+    if name.to_s =~ /^find_by_(.+)$/
+      hit = $1.end_with?('!')
+      str = hit ? $1.delete('!') : $1
+      cols = str.split('_and_')
       return super unless cols.all? {|c| User.method_defined?(c) }
       true
     else
